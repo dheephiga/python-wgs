b2635d6c4f1323b4f43511f88895dfff4bccf550
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2024-03-05 18:31:44 +0530

    adding files

---
+++ cache.py
@@ -0,0 +1,4 @@
+'''compiling a python file
+python -m py_compile <filename>'''
+
+print('wiuhfwhoeu')
---
+++ dis.py
@@ -0,0 +1,6 @@
+def hello():
+    print("Dis assembler")
+
+#to see how the disassembler works inside python
+import dis
+dis.dis(hello)
