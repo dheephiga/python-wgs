7291e91b103706cf42d855971ccdc107422faa95
dheephiga <anithavmane@gmail.com>
2024-03-11 19:23:26 +0530

    modified:   tasks/assessment-1/palindrome.py

--- tasks/assessment-1/palindrome.py
+++ tasks/assessment-1/palindrome.py
@@ -1,3 +1,17 @@
 user_input = input()
 palindrome = {}
+input_list = user_input.split()  
+def find_palindromes(in_list):
+    for word in in_list:
+        word_r = word[::-1] 
+        if word == word_r:
+            word_len = len(word)
+            
+            if word_len in palindrome:
+                palindrome[word_len].append(word)
+            else:
+                palindrome[word_len] = [word]  
 
+    print(palindrome)
+
+find_palindromes(input_list)
