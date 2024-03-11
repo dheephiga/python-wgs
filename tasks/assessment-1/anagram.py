c770e099a850ce47a8000d86cc1d13363fd794d8
dheephiga <anithavmane@gmail.com>
2024-03-11 18:17:59 +0530

    updated routes

---
+++ tasks/assessment-1/anagram.py
@@ -0,0 +1,2 @@
+raw_data = input()
+r_list = list(raw_data.split(','))
--- tasks/practice-1/assessment-1/anagram.py
+++
@@ -1,4 +0,0 @@
-'''Anagram Finder 
-input - list of words
-output - dictionary where keys are sorted letters of the word
-    values are anagrams -> from the list'''
