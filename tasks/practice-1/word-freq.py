dd8723b30ecfc35b29bddc3609a75d554e4b7ed1
dheephiga <anithavmane@gmail.com>
2024-03-13 20:04:35 +0530

    new file:   numpy/data.csv 	new file:   tasks/practice-1/word-freq.py

---
+++ tasks/practice-1/word-freq.py
@@ -0,0 +1,6 @@
+''' Write a function that takes a list of
+words as input and returns a dictionary where keys are unique words
+and values are their frequencies in the list.'''
+
+def frequency(r_list):
+    
