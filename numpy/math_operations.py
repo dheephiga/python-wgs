848455aaa2edcf5a1fc168bc7a6d262411289d1d
dheephiga <anithavmane@gmail.com>
2024-03-13 20:37:30 +0530

    new file:   numpy/math_operations.py 	modified:   numpy/pandas_practice.ipynb 	modified:   numpy/solved - 01-pandas_introduction.ipynb 	new file:   tasks/practice-1/arbitrary_sum.py 	new file:   tasks/practice-1/unique.py 	modified:   tasks/practice-1/word-freq.py

---
+++ numpy/math_operations.py
@@ -0,0 +1,4 @@
+'''Create a module called math_operations.py
+containing functions for addition, subtraction, multiplication, and
+division. Then write a program that imports this module and uses
+these functions.'''
---
+++ tasks/practice-1/arbitrary_sum.py
@@ -0,0 +1,4 @@
+def arb_sum(*num):
+    print(sum(num))
+
+arb_sum(1,3,6,9,5,645,65,654,64,65,67,65,465)
