46ab4da494ecbf81534e9344c57f624b0a706c36
dheephiga <anithavmane@gmail.com>
2024-03-15 19:13:03 +0530

    routes

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World!</h1>'
+def home():
+    return '<h1>Home page</h1>'
+
+
+@app.route('/about')
+def about():
+    return '<h1>About page</h1>'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+app.debug = True
 
 # @app.route('/hello/<user>')
 # def hello_name(user):
 #     return render_template('index.html', name=user)
-
+#
 # @app.route('/hello/<int:user>')
 # def hello_name(user):
 #     return render_template('index.html', marks=user)
-@app.route('/hello')
-def hello_name():
-    mydict = {'phy': 90, 'chem': 80, 'bio': 99}
-    return render_template('index.html', result=mydict)
+# @app.route('/hello')
+# def hello_name():
+#     mydict = {'phy': 90, 'chem': 80, 'bio': 99}
+#     return render_template('index.html', result=mydict)
 
+@app.route('/user/<user>')
+def user(user):
+    return 'hey user {}'.format(user)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
