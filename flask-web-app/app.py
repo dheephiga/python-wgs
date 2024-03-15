55a4dfd822251b4e2eb4ebe7018e86b25d28ec2a
dheephiga <anithavmane@gmail.com>
2024-03-15 22:04:57 +0530

    Templates

--- flask-web-app/app.py
+++ flask-web-app/app.py
@@ -1,17 +1,31 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
+app.debug = True
 
-
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Post-1',
+        'content': 'Hey there, just checking out this local app',
+        'date_posted': 'April 29, 2022'
+     },
+    {
+        'author': 'John Doe',
+        'title': 'Post-2',
+        'content': 'Hey there, just checking out bug fixes',
+        'date_posted': 'May 5, 2022'
+     }
+]
 @app.route('/')
-@app.route('home')
+@app.route('/home')
 def home():
-    return 'Home page!'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return 'About page'
+    return render_template('about.html', title='about')
 
 
 if __name__ == '__main__':
