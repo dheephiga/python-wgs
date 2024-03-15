f67a0a9436c82fa255b1488e8ca4d2ad3207027a
dheephiga <anithavmane@gmail.com>
2024-03-15 18:55:14 +0530

    jinja template syntax

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/hello/<int:user>')
-def hello_name(user):
-    return render_template('index.html', marks=user)
+# @app.route('/hello/<user>')
+# def hello_name(user):
+#     return render_template('index.html', name=user)
+
+# @app.route('/hello/<int:user>')
+# def hello_name(user):
+#     return render_template('index.html', marks=user)
+@app.route('/hello')
+def hello_name():
+    mydict = {'phy': 90, 'chem': 80, 'bio': 99}
+    return render_template('index.html', result=mydict)
 
 
 if __name__ == '__main__':
