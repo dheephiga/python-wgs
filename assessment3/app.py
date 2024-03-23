0c261887a074f64cff827c9773629146b19e8d7a
dheephiga <anithavmane@gmail.com>
2024-03-23 13:19:18 +0530

    modified:   assessment3/__pycache__/app.cpython-312.pyc 	modified:   assessment3/app.py 	new file:   assessment3/templates/answer.html 	modified:   assessment3/templates/home.html 	new file:   assessment3/templates/login.html 	new file:   assessment3/templates/login_info.html 	new file:   assessment3/templates/marks.html 	modified:   assessment3/templates/task1.html 	modified:   assessment3/templates/task2.html 	modified:   assessment3/templates/task3.html 	modified:   assessment3/templates/task4.html 	modified:   oop/oop.ipynb

--- assessment3/app.py
+++ assessment3/app.py
@@ -1,13 +1,54 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title='home')
 
+@app.route('/task1')
+def task1():
+    return render_template('task1.html')
+
+@app.route('/marks', methods=['POST'])
+def result():
+    physics_marks = int(request.form['physics'])
+    chemistry_marks = int(request.form['chemistry'])
+    math_marks = int(request.form['math'])
+
+    total_marks = physics_marks + chemistry_marks + math_marks
+    average_marks = total_marks / 3
+
+    return render_template('marks.html', physics=physics_marks, chemistry=chemistry_marks, math=math_marks,
+                           total=total_marks, average=average_marks)
+
+@app.route('/task2')
+def task2():
+    return render_template('task2.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    return render_template('login_info.html', username=username, password=password)
+
+@app.route('/task3')
+def task3():
+    return render_template('task3.html')
+
+def calculate_sum(n):
+    return sum(range(1, n + 1))
+
+@app.route('/sum', methods=['GET'])
+def sum_of_numbers():
+    number = int(request.args.get('number'))
+    total_sum = calculate_sum(number)
+    return render_template('answer.html', number=number, sum=total_sum)
+
+@app.route('/task4')
+def task4():
+    return render_template('task4.html')
 
 if __name__ == '__main__':
     app.run()
