8369144f45a8104f1e2908576ccd3841eb049c9d
dheephiga <anithavmane@gmail.com>
2024-04-08 22:59:33 +0530

    modified:   tutorial-app/app.py 	new file:   tutorial-app/templates/index3.html

--- tutorial-app/app.py
+++ tutorial-app/app.py
@@ -2,6 +2,27 @@ from flask import Flask, request,make_response,render_template,url_for,redirect
 
 app = Flask(__name__)
 
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index3.html')
+    elif request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == 'admin' and password == 'admin':
+            return 'success'
+        else:
+            return 'fail'
+
+
+
+
+
+
+
+
+
 # @app.route('/')
 # def index():
 #     return "hey"
@@ -31,33 +52,33 @@ app = Flask(__name__)
 #         return f'{greeting}, {name}'
 #     else:
 #         return 'parameters missing'
-@app.route('/')
-def index():
-    v1 = 'neuron'
-    result = 10 + 393
-    myl = [10,90,44,89,45]
-    return render_template('index.html',v1=v1,result=result,list=myl)
+# @app.route('/')
+# def index():
+#     v1 = 'neuron'
+#     result = 10 + 393
+#     myl = [10,90,44,89,45]
+#     return render_template('index.html',v1=v1,result=result,list=myl)
 
-@app.route('/nope')
-def other():
-    text = 'Hello Python'
-    return render_template('index2.html',text=text)
+# @app.route('/nope')
+# def other():
+#     text = 'Hello Python'
+#     return render_template('index2.html',text=text)
 
-@app.route('/redirect')
-def redirect_point():
-    return redirect(url_for('other'))
+# @app.route('/redirect')
+# def redirect_point():
+#     return redirect(url_for('other'))
 
-@app.template_filter('reverse_string')
-def reverse_string(s):
-    return s[::-1]
+# @app.template_filter('reverse_string')
+# def reverse_string(s):
+#     return s[::-1]
 
-@app.template_filter('repeat')
-def repeat(s,times=2):
-    return s*times
+# @app.template_filter('repeat')
+# def repeat(s,times=2):
+#     return s*times
 
-@app.template_filter('alternate_case')
-def alternate_case(s):
-    return ''.join([c.upper() if i%2==0 else c.lower() for i,c in enumerate(s)])
+# @app.template_filter('alternate_case')
+# def alternate_case(s):
+#     return ''.join([c.upper() if i%2==0 else c.lower() for i,c in enumerate(s)])
 
 
 if __name__ == '__main__':
