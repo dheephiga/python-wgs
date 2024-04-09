6204be0b84429bb8a23f3bf57548a93bdfaa381b
dheephiga <anithavmane@gmail.com>
2024-04-09 20:21:41 +0530

    modified:   pushup-logger/app.py 	new file:   pushup-logger/static/vector1.jpg 	modified:   pushup-logger/templates/base.html 	modified:   pushup-logger/templates/home.html

--- pushup-logger/app.py
+++ pushup-logger/app.py
@@ -1,1 +1,18 @@
-from flask import FLask, render_template,request,Response,url_for
+from flask import Flask, render_template,request,Response,url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    pass
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    pass
+if __name__ == '__main__':
+    app.run(debug=True)
