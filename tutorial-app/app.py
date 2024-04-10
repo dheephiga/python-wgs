102beb79f6ee42590637ff6214bc824a546f85f1
dheephiga <anithavmane@gmail.com>
2024-04-11 00:45:38 +0530

    modified:   tutorial-app/app.py 	modified:   tutorial-app/templates/index3.html

--- tutorial-app/app.py
+++ tutorial-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,make_response,render_template,url_for,redirect, Response, send_from_directory
+from flask import Flask, request,make_response,render_template,url_for,redirect, Response, send_from_directory, jsonify
 import pandas as pd
 import os
 import uuid
@@ -59,14 +59,24 @@ def convert_csv_two():
 @app.route('/download/<filename>')
 def download(filename):
     return send_from_directory('downloads',filename,download_name='result.csv')
-    
 
+@app.route('/handle_post',methods=['POST'])
+def handle_post():
+    greeting = request.json['greeting']
+    name = request.json['name']
+    
+    with open('file.txt','w') as f:
+        f.write(f'{greeting}, {name}')
+    
+    return  jsonify({'message':'successfully written'})
+    
+    
 # @app.route('/')
 # def index():
 #     return "hey"
 
 # @app.route('/hello')
-# def hello():
+# def hello():  
 # #    return 'helloWorld', 201
 #     response = make_response('hola')
 #     response.status_code = 202
