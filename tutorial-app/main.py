2aba9e5e80b868e320df9f379af81804945a6246
dheephiga <anithavmane@gmail.com>
2024-04-10 18:13:37 +0530

    modified:   tutorial-app/main.py

--- tutorial-app/main.py
+++ tutorial-app/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api,reqparse
 
 app = Flask("VideoAPI")
 api = Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument('title',required=True)
 videos = {
     'video1': {'title':'Python introduction'},
     'video2': {'title':'Matlab vs Python'}
@@ -15,8 +17,15 @@ class Video(Resource):
         if video_id == "all":
             return videos
         return videos[video_id]
+    
+    def put(self,video_id):
+        args = parser.parse_args()
+        new_video = {'title':args['title']}
+        videos[video_id] = new_video
+        return {video_id: videos[video_id]}, 201    
 
 api.add_resource(Video,'/videos/<video_id>')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
