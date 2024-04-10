3e41b705951fa57e87c12ed0f353c7fc873790d8
dheephiga <anithavmane@gmail.com>
2024-04-10 16:58:02 +0530

    modified:   tutorial-app/main.py

--- tutorial-app/main.py
+++ tutorial-app/main.py
@@ -4,3 +4,19 @@ from flask_restful import Resource, Api
 app = Flask("VideoAPI")
 api = Api(app)
 
+videos = {
+    'video1': {'title':'Python introduction'},
+    'video2': {'title':'Matlab vs Python'}
+}
+
+class Video(Resource):
+    
+    def get(self,video_id):
+        if video_id == "all":
+            return videos
+        return videos[video_id]
+
+api.add_resource(Video,'/videos/<video_id>')
+
+if __name__ == "__main__":
+    app.run(debug=True)
