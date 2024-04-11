f710af4fbfa24f986f705eeeeb6501851f7b6d32
dheephiga <anithavmane@gmail.com>
2024-04-11 17:58:19 +0530

    modified:   login-app/app.py 	modified:   login-app/routes.py

--- login-app/app.py
+++ login-app/app.py
@@ -2,20 +2,35 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_bcrypt import bcrypt
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./logindb.db'
+    app.secret_key = 'KEY'
 
+    # Initialize extensions
     db.init_app(app)
+    bcrypt.init_app(app)
 
-
+    # Register routes and other configurations
     from routes import register_routes
-    register_routes(app,db)
-    migrate = Migrate(app,db)
+    register_routes(app, db, bcrypt)
+
+    # Initialize Flask-Migrate
+    migrate = Migrate(app, db)
+
+    # Initialize Flask-Login
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    # Load user function for Flask-Login
+    from models import User
+    @login_manager.user_loader
+    def load_user(uid):
+        return User.query.get(int(uid))
 
     return app
--- login-app/routes.py
+++ login-app/routes.py
@@ -1,8 +1,24 @@
 from flask import render_template, request
 from models import User
+from flask_login import login_user, logout_user, current_user, login_required
 
-def register_routes(app,db):
 
-    @app.route('/',methods=['GET','POST'])
+def register_routes(app, db, bcrypt):
+    @app.route('/', methods=['GET', 'POST'])
     def index():
-        pass
+        if current_user.is_authenticated:
+            return str(current_user.username)
+        else:
+            return 'No user logged in'
+
+    @app.route('/login/<uid>')
+    def login(uid):
+        user = User.query.get(uid)
+        login_user(user)
+        return 'success'
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        return 'success'
+
