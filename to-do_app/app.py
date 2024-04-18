46fa5985d67a91079db2051d95a12786e140afe6
dheephiga <anithavmane@gmail.com>
2024-04-18 12:41:02 +0530

    modified:   to-do_app/app.py 	new file:   to-do_app/templates/edit.html 	modified:   to-do_app/templates/index.html

--- to-do_app/app.py
+++ to-do_app/app.py
@@ -8,5 +8,19 @@ todos = [{'todo': 'code', 'done': False}]
 def index():
     return render_template('index.html',todos=todos)
 
+@app.route('/add', methods=['POST'])
+def add():
+    todo = request.form['todo']
+    todos.append({'todo':todo, 'done':False})
+    return redirect(url_for('index'))
+
+def edit(index):
+    todo = todos[index]
+    if request.method == 'POST':
+        todo['todo'] = request.formt['todo']
+        return redirect(url_for('index'))
+    else:
+        render_template('edit.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
