317adc4e5837f2fc5e10fb11e47111ec8302e2f9
dheephiga <anithavmane@gmail.com>
2024-04-19 15:32:38 +0530

    modified:   project-2/app.py 	modified:   project-2/templates/navbar.html 	modified:   project-2/templates/user.html

--- project-2/app.py
+++ project-2/app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,request
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
 
 @app.route('/')
