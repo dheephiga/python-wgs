89ce7097211c4059b9758ef0b624be66165224de
dheephiga <anithavmane@gmail.com>
2024-04-19 16:41:35 +0530

    modified:   project-2/app.py 	new file:   project-2/templates/name.html 	modified:   project-2/templates/navbar.html

--- project-2/app.py
+++ project-2/app.py
@@ -2,8 +2,13 @@ from flask import Flask,render_template,request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.secret_key = 'secret key'
+
+class NameForm(FlaskForm):
+    name = StringField("What's your name",validators=[DataRequired()])
+    submit = SubmitField("Submit")
 @app.route('/')
 def index():
     firstname = 'John'
@@ -23,5 +28,14 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template('500.html'), 500
 
+@app.route('/name',methods=['GET','POST'])
+def name():
+    name = None
+    form = NameForm()
+    
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html',name=name,form=form)
 if __name__ == '__main__':
     app.run(debug=True)
