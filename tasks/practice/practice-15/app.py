5eac67de7f6b3cf89c3ee165d21c28920b13df00
dheephiga <anithavmane@gmail.com>
2024-04-20 19:01:03 +0530

    modified:   tasks/practice/practice-15/app.py 	modified:   tasks/practice/practice-15/templates/index.html

--- tasks/practice/practice-15/app.py
+++ tasks/practice/practice-15/app.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, flash, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, ValidationError
 import numpy as np
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 
 class InputForm(FlaskForm):
-    numbers = TextAreaField('Numbers', validators=[InputRequired(message='Please enter numbers')])
+    numbers = TextAreaField('Numbers', validators=[
+        InputRequired(message='Please enter numbers'),
+    ])
     submit = SubmitField('Calculate')
+
+    def validate_numbers(form, field):
+        try:
+            # Split the input by commas or spaces and convert each part to float
+            numbers = [float(x) for x in field.data.replace(',', ' ').split()]
+        except ValueError:
+            raise ValidationError('Please enter valid numbers')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,7 +28,7 @@ def index():
     std = None
     if form.validate_on_submit():
         try:
-            num = np.array(form.numbers.data.split(), dtype=float)
+            num = np.array([float(x) for x in form.numbers.data.split()], dtype=float)
             std = np.std(num)
             mean = np.mean(num)
         except ValueError:
