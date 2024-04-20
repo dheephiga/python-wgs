460d2dea08d66c72122c55ced95ebafd583971ce
dheephiga <anithavmane@gmail.com>
2024-04-20 19:20:55 +0530

    modified:   tasks/practice/practice-15/app.py 	modified:   tasks/practice/practice-15/templates/index.html

--- tasks/practice/practice-15/app.py
+++ tasks/practice/practice-15/app.py
@@ -15,8 +15,8 @@ class InputForm(FlaskForm):
 
     def validate_numbers(form, field):
         try:
-            # Split the input by commas or spaces and convert each part to float
-            numbers = [float(x) for x in field.data.replace(',', ' ').split()]
+            # Split the input by any whitespace character and convert each part to float
+            numbers = [float(x) for x in field.data.split()]
         except ValueError:
             raise ValidationError('Please enter valid numbers')
 
@@ -26,15 +26,20 @@ def index():
     num = None
     mean = None
     std = None
+
     if form.validate_on_submit():
         try:
             num = np.array([float(x) for x in form.numbers.data.split()], dtype=float)
             std = np.std(num)
             mean = np.mean(num)
+            print("Mean:", mean)
+            print("Standard Deviation:", std)
         except ValueError:
             flash('Please enter valid numbers', 'error')
             return redirect(url_for('index'))
+
     return render_template('index.html', form=form, num=num, mean=mean, std=std)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
