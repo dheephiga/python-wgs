f3fa8b6df55e9e171e3f7926bd947849867c7baf
dheephiga <anithavmane@gmail.com>
2024-04-21 11:09:17 +0530

    new file:   Your-Chef/app.py 	new file:   Your-Chef/templates/add_recipe.html 	new file:   Your-Chef/templates/base.html 	new file:   Your-Chef/templates/index.html 	new file:   Your-Chef/templates/update_recipe.html 	modified:   tutorial-app/main.py

--- tutorial-app/main.py
+++ tutorial-app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api,reqparse
+from flask_restful import Resource, Api,reqparse, abort
 
 app = Flask("VideoAPI")
 api = Api(app)
@@ -16,13 +16,19 @@ class Video(Resource):
     def get(self,video_id):
         if video_id == "all":
             return videos
-        return videos[video_id]
+        return videos[video_id],
     
     def put(self,video_id):
         args = parser.parse_args()
         new_video = {'title':args['title']}
         videos[video_id] = new_video
         return {video_id: videos[video_id]}, 201    
+    
+    def delete(self, video_id):
+        if video_id not in videos:
+            abort(404, message=f'Video {video_id} not found')
+        del videos[video_id]
+        return "", 204
 
 api.add_resource(Video,'/videos/<video_id>')
 
