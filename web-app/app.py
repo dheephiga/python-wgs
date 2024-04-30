8f8c97fb6dc14360ef2243ac6477d5301f5bc3d7
dheephiga <anithavmane@gmail.com>
2024-04-30 19:30:37 +0530

    modified:   web-app/app.py 	modified:   web-app/templates/about.html 	modified:   web-app/templates/index.html

--- web-app/app.py
+++ web-app/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 
-app =  Flask(__name__)
+dodo =  Flask(__name__)
 
-@app.route('/')
+@dodo.route('/')
 def index():
-    return render_template('index.html')
+    title = 'index'
+    return render_template('index.html', title=title)
 
-@app.route('/about')
+@dodo.route('/about')
 def about():
-    return render_template('about.html')
+    names = ['Meredith', 'Yang', 'George', 'Izzy', 'Alex']
+    return render_template('about.html', names=names)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   dodo.run(debug=True)
